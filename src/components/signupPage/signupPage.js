28fab25ae6bfc878ccb978e4cc9aa91a5baa70cb
esvetlakov <109486102+esvetlakov@users.noreply.github.com>
2023-02-04 18:28:30 +0300

    signup page on useForm hook

--- src/components/signupPage/signupPage.js
+++ src/components/signupPage/signupPage.js
@@ -1,81 +1,114 @@
-import { Checkbox } from 'antd';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 import classes from './signupPage.module.scss';
 
 function SignupPage() {
-  const [userName, setUserName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
-  const [agreed, setAgreed] = useState(false);
-  /*
-  const [pwdLengthErr, setPwdLengthErr] = useState(false);
-  const [pwdMisMatch, setPwdMisMatch] = useState(false);
-*/
+  const {
+    register,
+    watch,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    mode: 'onBlur',
+  });
 
-  const onChange = (e) => {
-    const { target } = e;
-    const { value, checked } = target;
-    switch (target.name) {
-      case 'username':
-        setUserName(value);
-        break;
-      case 'email':
-        setEmail(value.toLowerCase());
-        break;
-      case 'password':
-        setPassword(value);
-        break;
-      case 'repeatPassword':
-        setRepeatPassword(value);
-        break;
-      case 'agreed':
-        setAgreed(checked);
-        break;
-      default:
-        return null;
-    }
-    return null;
+  const onSubmit = (data) => {
+    console.log(data);
   };
 
-  /*
-  const onSubmit = () => {
-
-  };
- */
   return (
     <div className={classes.wrap}>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
         <h2 className={classes.title}>Create new account</h2>
-        <label className={classes.label}>
+        <label className={!errors.userName ? classes.label : `${classes.label} ${classes.err}`}>
           Username
-          <input type="text" name="username" placeholder="Username" value={userName} onChange={onChange} required />
+          <input
+            type="text"
+            placeholder="Username"
+            {...register('userName', {
+              required: 'Username is required',
+              minLength: {
+                value: 3,
+                message: 'Username length should be 3 to 20 characters',
+              },
+              maxLength: {
+                value: 20,
+                message: 'Username length should be 3 to 20 characters',
+              },
+              pattern: {
+                value: /^[a-z][a-z0-9]*$/,
+                message: 'You can only use lowercase English letters and numbers',
+              },
+            })}
+          />
+          <div className={classes.errMsg}>
+            {errors?.userName && <span>{errors?.userName?.message || 'Error'}</span>}
+          </div>
         </label>
-        <label className={classes.label}>
+        <label className={!errors.email ? classes.label : `${classes.label} ${classes.err}`}>
           Email
-          <input type="email" name="email" placeholder="Email address" value={email} onChange={onChange} required />
+          <input
+            type="email"
+            style={{ textTransform: 'lowercase' }}
+            placeholder="Email address"
+            {...register('email', {
+              required: 'Email is required',
+            })}
+          />
+          <div className={classes.errMsg}>{errors?.email && <span>{errors?.email?.message || 'Error'}</span>}</div>
         </label>
-        <label className={classes.label}>
+        <label className={!errors.password ? classes.label : `${classes.label} ${classes.err}`}>
           Password
-          <input type="password" name="password" placeholder="Password" value={password} onChange={onChange} required />
+          <input
+            type="password"
+            placeholder="Password"
+            {...register('password', {
+              required: 'Password is required',
+              minLength: {
+                value: 6,
+                message: 'Password length should be 6 to 40 characters',
+              },
+              maxLength: {
+                value: 40,
+                message: 'Password length should be 6 to 40 characters',
+              },
+            })}
+          />
+          <div className={classes.errMsg}>
+            {errors?.password && <span>{errors?.password?.message || 'Error'}</span>}
+          </div>
         </label>
-        <label className={classes.label}>
+        <label className={!errors.repeatPassword ? classes.label : `${classes.label} ${classes.err}`}>
           Repeat password
           <input
             type="password"
-            name="repeatPassword"
             placeholder="Password"
-            value={repeatPassword}
-            onChange={onChange}
-            required
+            {...register('repeatPassword', {
+              required: 'Repeat password is required',
+              validate: (val) => {
+                if (watch('password') !== val) return 'Passwords must match';
+                return null;
+              },
+              minLength: {
+                value: 6,
+                message: 'Password length should be 6 to 40 characters',
+              },
+              maxLength: {
+                value: 40,
+                message: 'Password length should be 6 to 40 characters',
+              },
+            })}
           />
+          <div className={classes.errMsg}>
+            {errors?.repeatPassword && <span>{errors?.repeatPassword?.message || 'Error'}</span>}
+          </div>
         </label>
 
-        <Checkbox className={classes.labelCheckBox} name="agreed" checked={agreed} onChange={onChange} required>
-          I agree to the processing of my personal information
-        </Checkbox>
+        <label className={classes.labelCheckBox}>
+          <input type="checkbox" className={classes.checkbox} required />
+          <span className={classes.checkboxText}>I agree to the processing of my personal information</span>
+        </label>
         <input type="submit" className={classes.submit} value="Create" />
         <div className={classes.signin}>
           Already have an account?{' '}
